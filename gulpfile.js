933a3570ee6dc95d70697029527bff26acdf3960
Brian Manderino <bmanderi@yahoo.com>
2016-04-28 13:23:31 -0700

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
-    autoprefix = require('gulp-autoprefixer');
+    autoprefix = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    jshint = require('gulp-jshint');
 
  gulp.task('styles', function() {
  	return gulp.src('css/*.scss')
@@ -9,10 +11,16 @@ var gulp = require('gulp'),
  		'sourcemap=none': true
  	}))
  	.pipe(concat('custom.css'))
- 	.pipe(autoprefix())
- 	.pipe(gulp.dest('css/'))
+ 	.pipe(autoprefix('last 2 version'))
+ 	.pipe(cleanCSS())
+ 	.pipe(gulp.dest('dist/css/'))
+});
+gulp.task('jshint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
 });
 gulp.task('watch', function() {
     gulp.watch('css/*.scss', ['styles']);
+    gulp.watch('js/*js', ['jshint']);
 });
-gulp.task('default', ['styles','watch']);
+gulp.task('default', ['styles', 'jshint','watch']);
