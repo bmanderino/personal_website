333cd71c745595c6378634b363deecc4f2e1d283
Brian Manderino <bmanderi@yahoo.com>
2016-04-28 15:51:45 -0700

    src and dist

--- gulpfile.js
+++ gulpfile.js
@@ -6,34 +6,35 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint');
 
  gulp.task('styles', function() {
- 	return gulp.src('css/*.scss')
+ 	return gulp.src('src/css/*.scss')
  	.pipe(sass({
  		'sourcemap=none': true
  	}))
  	.pipe(concat('custom.css'))
  	.pipe(autoprefix('last 2 version'))
  	.pipe(cleanCSS())
- 	.pipe(gulp.dest('css/'))
+ 	.pipe(gulp.dest('src/css/'))
 });
 gulp.task('jshint', function() {
     return gulp.src('js/*.js')
         .pipe(jshint())
 });
 gulp.task('watch', function() {
-    gulp.watch('css/*.scss', ['styles']);
-    gulp.watch('js/*js', ['jshint']);
+    gulp.watch('src/css/*.scss', ['styles']);
+    gulp.watch('src/js/*js', ['jshint']);
 });
 gulp.task('default', ['styles', 'jshint','watch']);
 
 
 var moveFiles = [
-	'index.html',
-	'./css/*.css',
-	'./js/*',
-	'./img/*',
-	'./portfolio/*'
+	'src/index.html',
+	'src/css/*.css',
+	'src/js/*',
+	'src/img/*',
+	'src/portfolio/*',
+    'src/fonts/*'
 ];
 gulp.task('deploy', function() {
-	return gulp.src(moveFiles, {base: './'})
+	return gulp.src(moveFiles, {base: 'src/'})
 		.pipe(gulp.dest('dist'))
 });
